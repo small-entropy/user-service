1d5932610adc63095c5eaa81f2a85daa90ddc33d
Dmitrii Shevelev <igrave1988@gmail.com>
2021-02-13 16:26:53 +0500

    Simplyfied profile part of code

--- src/api/ApiRegistration/api_registration.service.ts
+++ src/api/ApiRegistration/api_registration.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from '../../common/dto/create_user.dto';
 import { UserService } from '../../database/User/user.service';
 import { UserPropertyService } from '../../database/UserProperty/user_property.service';
-import { DEFAULT_PROFILE_FIELDS } from '../../common/constants/profile.constants';
+import {
+  DEFAULT_PROFILE_FIELDS,
+  DEFAULT_PROFILE_FIELDS_VALUES,
+} from '../../common/constants/profile.constants';
 @Injectable()
 export class RegisterService {
   constructor(
@@ -11,50 +14,40 @@ export class RegisterService {
     private userPropertyService: UserPropertyService,
   ) {}
 
+  private getProfilePromises(uuid) {
+    return DEFAULT_PROFILE_FIELDS.map((field) => {
+      return new Promise((resolve, reject) => {
+        try {
+          const userProperty = this.userPropertyService.create(
+            uuid,
+            field,
+            DEFAULT_PROFILE_FIELDS_VALUES[field]
+              ? DEFAULT_PROFILE_FIELDS_VALUES[field]
+              : 'empty',
+          );
+          resolve(userProperty);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+  }
+
+  private getProfileRecord(properties) {
+    const profile = {};
+    properties.forEach((property: Record<string, any>) => {
+      profile[property.name] = property.value;
+    });
+    return profile;
+  }
+
   async register(createUserDto: CreateUserDTO) {
     try {
       const user = await this.userService.create(createUserDto);
       const uuid = user._id;
-      let property_value;
-      const profile_promises = DEFAULT_PROFILE_FIELDS.map((field) => {
-        return new Promise((resolve, reject) => {
-          try {
-            switch (field) {
-              case 'sex':
-                property_value = 'Unknown sex';
-                break;
-              case 'firstName':
-                property_value = 'First name unknown';
-                break;
-              case 'middleName':
-                property_value = 'Middle name unknown';
-                break;
-              case 'lastName':
-                property_value = 'Last name unknown';
-                break;
-              case 'description':
-                property_value = 'Description not filled in';
-                break;
-              default:
-                property_value = 'empty';
-                break;
-            }
-            const userProperty = this.userPropertyService.create(
-              uuid,
-              field,
-              property_value,
-            );
-            resolve(userProperty);
-          } catch (error) {
-            reject(error);
-          }
-        });
-      });
+      const profile_promises = this.getProfilePromises(uuid);
       const results = await Promise.all(profile_promises);
-      const profile = {};
-      results.forEach((property: Record<string, any>) => {
-        profile[property.name] = property.value;
-      });
+      const profile = this.getProfileRecord(results);
       return { user, profile };
     } catch (error) {
       throw error;
--- src/common/constants/profile.constants.ts
+++ src/common/constants/profile.constants.ts
@@ -6,3 +6,11 @@ export const DEFAULT_PROFILE_FIELDS = [
   'lastName',
   'description',
 ];
+
+export const DEFAULT_PROFILE_FIELDS_VALUES = {
+  sex: 'Unknown sex',
+  firstName: 'First name unknown',
+  middleName: 'Middle name unknown',
+  lastName: 'Last name unknown',
+  description: 'Description not filled in',
+};
