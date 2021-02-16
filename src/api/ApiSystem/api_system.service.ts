03660f692eae8172524aa33ff9a32506bec87c87
Dmitrii Shevelev <igrave1988@gmail.com>
2021-02-16 16:06:39 +0500

    Add documentation

--- src/api/ApiSystem/api_system.service.ts
+++ src/api/ApiSystem/api_system.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ApiSystemService {
+  /**
+   * Method for get server health
+   */
   getHealt() {
     return { status: 'ok' };
   }
